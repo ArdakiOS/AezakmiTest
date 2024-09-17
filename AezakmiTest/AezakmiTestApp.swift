4093946fff4a20c9df7b6bdfd930dc0e5dde8b63
Ardak <ardaktursunbayev4@gmail.com>
2024-09-17 14:06:30 +0500

    Initial Commit

---
+++ AezakmiTest/AezakmiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AezakmiTestApp.swift
+//  AezakmiTest
+//
+//  Created by Ardak Tursunbayev on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AezakmiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
